4787f88db058bcc621021c7b1ce5c3357ab059f6
이경훈 <leta6603@gmail.com>
2021-03-31 16:58:18 +0900

    Change logo and app name.

---
+++ app/src/main/java/me/lkhz/memoduck/MainActivity.java
@@ -0,0 +1,14 @@
+package me.lkhz.memoduck;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
